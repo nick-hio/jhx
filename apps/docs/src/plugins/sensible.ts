88a1b4554b010dc20c48f0ee64e48c47490d7ad8
nick-hio <209283586+nick-hio@users.noreply.github.com>
2025-11-11 17:11:07 -0400

    Started `apps/docs`.

---
+++ apps/docs/src/plugins/sensible.ts
@@ -0,0 +1,7 @@
+import fp from 'fastify-plugin';
+import sensible from '@fastify/sensible';
+import type { SensibleOptions } from '@fastify/sensible';
+
+export default fp<SensibleOptions>(async (fastify) => {
+    fastify.register(sensible)
+})
