934eacd1dba655f48e75559bd21a656e2986ff6a
nick-hio <209283586+nick-hio@users.noreply.github.com>
2025-11-11 23:26:40 -0400

    Created 'Under Construction' page for the docs site.

--- apps/docs/src/server.ts
+++ apps/docs/src/server.ts
@@ -1,15 +1,23 @@
 import Fastify from 'fastify';
 import AutoLoad from '@fastify/autoload';
-import { join } from 'path';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
 
+let dirName;
+// @ts-ignore: `__dirname` ESM fix
+if (process.env.NODE_ENV === 'development' || typeof __dirname === 'undefined') {
+    dirName = dirname(fileURLToPath(import.meta.url));
+} else {
+    dirName = __dirname;
+}
 
-const fastify = Fastify({ logger: true })
+const fastify = Fastify({ logger: true });
 
 fastify.register(AutoLoad, {
-    dir: join(__dirname, 'plugins'),
-})
+    dir: join(dirName, 'plugins'),
+});
 fastify.register(AutoLoad, {
-    dir: join(__dirname, 'routes'),
-})
+    dir: join(dirName, 'routes'),
+});
 
 fastify.listen({ port: 3000 })
