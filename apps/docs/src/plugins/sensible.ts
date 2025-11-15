11e98740dcdff02511d515c747653c8a36f16430
nick-hio <209283586+nick-hio@users.noreply.github.com>
2025-11-14 23:59:07 -0400

    Created an 'Under Construction' page for the docs site. Added `assets/jhx-logo-textless.svg`. Updated package descriptions.

--- apps/docs/src/plugins/sensible.ts
+++
@@ -1,7 +0,0 @@
-import fp from 'fastify-plugin';
-// import sensible from '@fastify/sensible';
-import type { SensibleOptions } from '@fastify/sensible';
-
-export default fp<SensibleOptions>(async (_fastify) => {
-    // fastify.register(sensible)
-})
