b90198579795ee661cf7acff4027045af993271f
nick-hio <209283586+nick-hio@users.noreply.github.com>
2025-11-14 23:59:07 -0400

    Created an 'Under Construction' page for the docs site. Added `assets/jhx-logo-textless.svg`. Updated package descriptions.

---
+++ apps/docs/src/plugins/jhx.ts
@@ -0,0 +1,6 @@
+// import fp from 'fastify-plugin'
+// import { fastifyJhx } from '@jhxdev/fastify'
+//
+// export default fp(async (fastify) => {
+//     await fastify.register(fastifyJhx)
+// })
--- apps/docs/src/server.ts
+++ apps/docs/src/server.ts
@@ -3,21 +3,16 @@ import AutoLoad from '@fastify/autoload';
 import { join, dirname } from 'path';
 import { fileURLToPath } from 'url';
 
-let dirName;
-// @ts-ignore: `__dirname` ESM fix
-if (process.env.NODE_ENV === 'development' || typeof __dirname === 'undefined') {
-    dirName = dirname(fileURLToPath(import.meta.url));
-} else {
-    dirName = __dirname;
-}
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const fastify = Fastify({ logger: true });
 
+// fastify.register(AutoLoad, {
+//     dir: join(__dirname, 'plugins'),
+// });
 fastify.register(AutoLoad, {
-    dir: join(dirName, 'plugins'),
-});
-fastify.register(AutoLoad, {
-    dir: join(dirName, 'routes'),
+    dir: join(__dirname, 'routes'),
 });
 
 fastify.listen({ port: 3000 })
